c41fee022c3a11dd74f06e373d6b8b72fe767fcb
Mark Laane <marklaane@gmail.com>
2015-10-24 00:56:07 +0300

    Added Test project - all looks fine

---
+++ Tests/Tests1.cpp
@@ -0,0 +1,9 @@
+#include <boost/test/unit_test.hpp>
+#include "ParticleFilterLocalizer.h"
+
+BOOST_AUTO_TEST_SUITE(ParticleFilterLocalizerTests)
+BOOST_AUTO_TEST_CASE(SomethingP)
+{
+	BOOST_CHECK(true);
+}
+BOOST_AUTO_TEST_SUITE_END()
