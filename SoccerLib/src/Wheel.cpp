085b1368ef3d22697491606b2ec0e220f95a88b0
user <user>
2015-11-12 21:50:06 +0200

    added new conf for wheel speeds to json as well as 001TRT conf

--- SoccerLib/src/Wheel.cpp
+++ SoccerLib/src/Wheel.cpp
@@ -6,10 +6,11 @@
 
 #include <iostream>
 
-const float Wheel::pidFrequency = 60.0f;
-const float Wheel::ticksPerRevolution = 64.0f * 18.75f;
+//replaced to conf json
+//const float Wheel::pidFrequency = 60.0f;
+//const float Wheel::ticksPerRevolution = 64.0f * 18.75f;
 
-Wheel::Wheel(int id) : id(id), targetOmega(0), filteredTargetOmega(0), realOmega(0), stallCounter(0) {
+Wheel::Wheel(int id, int maxSpeed, float wheelSpeedToMetric) : id(id), maxSpeed(maxSpeed), wheelSpeedToMetric(wheelSpeedToMetric), targetOmega(0), filteredTargetOmega(0), realOmega(0), stallCounter(0) {
     
 }
 
@@ -18,7 +19,7 @@ void Wheel::setTargetOmega(float omega) {
 }
 
 void Wheel::setTargetSpeed(int speed) {
-    setTargetOmega(speedToOmega((float)speed));
+    setTargetOmega(speedToOmega((float)speed, this->wheelSpeedToMetric));
 }
 
 float Wheel::getTargetOmega() const {
@@ -31,7 +32,7 @@ float Wheel::getFilteredTargetOmega() const {
 
 float Wheel::getTargetSpeed() const {
 	//return omegaToSpeed(getTargetOmega());
-	return omegaToSpeed(getFilteredTargetOmega());
+	return omegaToSpeed(getFilteredTargetOmega(), this->maxSpeed, this->wheelSpeedToMetric);
 }
 
 float Wheel::getRealOmega() const {
@@ -65,7 +66,7 @@ void Wheel::step(float dt) {
 bool Wheel::handleCommand(const Command& cmd) {
 	if (cmd.name == "speeds") {
 		if ((int)cmd.parameters.size() > id) {
-			realOmega = speedToOmega((float)Util::toInt(cmd.parameters[id]));
+			realOmega = speedToOmega((float)Util::toInt(cmd.parameters[id]), this->wheelSpeedToMetric);
 		} else {
 			std::cout << "- Invalid speeds info: " << cmd.name << " " << Util::toString(cmd.parameters) << std::endl;
 		}
@@ -87,10 +88,10 @@ bool Wheel::handleCommand(const Command& cmd) {
     return stream.str();
 }*/
 
-float Wheel::omegaToSpeed(float omega) {
-    return Math::limit(omega / Math::TWO_PI * ticksPerRevolution / pidFrequency, 255);
+float Wheel::omegaToSpeed(float omega, const int maxSpeed, const float wheelSpeedToMetric) {
+    return Math::limit(omega / Math::TWO_PI * wheelSpeedToMetric, (float)maxSpeed);
 }
 
-float Wheel::speedToOmega(float speed) {
-    return speed * pidFrequency / ticksPerRevolution * Math::TWO_PI;
+float Wheel::speedToOmega(float speed, const float wheelSpeedToMetric) {
+    return speed / wheelSpeedToMetric * Math::TWO_PI;
 }
